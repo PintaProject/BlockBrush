54f1358eab7b79127c2dd9c936f1c76dad839bed
Cameron White <cameronwhite91@gmail.com>
2013-06-15 19:45:30 -0400

    Minor documentation update.

--- BlockBrush/BlockBrush.cs
+++ BlockBrush/BlockBrush.cs
@@ -50,6 +50,7 @@ namespace BlockBrush
 		/// <param name="y">The current y coordinate of the mouse.</param>
 		/// <param name="lastX">The previous x coordinate of the mouse.</param>
 		/// <param name="lastY">The previous y coordinate of the mouse.</param>
+		/// <returns>A rectangle containing the area of the canvas that should be redrawn.</returns>
 		protected override Gdk.Rectangle OnMouseMove (Cairo.Context g, Cairo.Color strokeColor,
 		                                              Cairo.ImageSurface surface,int x, int y,
 		                                              int lastX, int lastY)
