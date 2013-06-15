0ac3ccf4488b7a13d749a96ae65ea78f485944b7
Cameron White <cameronwhite91@gmail.com>
2013-06-15 19:04:38 -0400

    Copy over the documentation from the BaseBrush class, since users may look at this add-in as an example.

--- BlockBrush/BlockBrush.cs
+++ BlockBrush/BlockBrush.cs
@@ -32,10 +32,24 @@ namespace BlockBrush
 {
 	public class BlockBrush : BasePaintBrush
 	{
+		/// <summary>
+		/// The name of the brush.
+		/// </summary>
 		public override string Name {
 			get { return AddinManager.CurrentLocalizer.GetString ("Block"); }
 		}
 
+		/// <summary>
+		/// Event handler called when the mouse is moved. This method is where
+		/// the brush should perform its drawing.
+		/// </summary>
+		/// <param name="g">The current Cairo drawing context.</param>
+		/// <param name="strokeColor">The current stroke color.</param>
+		/// <param name="surface">Image surface to draw on.</param>
+		/// <param name="x">The current x coordinate of the mouse.</param>
+		/// <param name="y">The current y coordinate of the mouse.</param>
+		/// <param name="lastX">The previous x coordinate of the mouse.</param>
+		/// <param name="lastY">The previous y coordinate of the mouse.</param>
 		protected override Gdk.Rectangle OnMouseMove (Cairo.Context g, Cairo.Color strokeColor,
 		                                              Cairo.ImageSurface surface,int x, int y,
 		                                              int lastX, int lastY)
